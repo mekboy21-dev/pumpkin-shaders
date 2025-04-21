f6405cbd362e6bc8d399a4876046b9db86e4f98f
mekboy21-dev <playmekstudios@gmail.com>
2025-04-21 17:23:22 +0100

    slight fixes
    - Fixed an issue where the new shadow code caused brightness issues with certain light sources.

--- shaders/common/solid_fragment.glsl
+++ shaders/common/solid_fragment.glsl
@@ -45,8 +45,8 @@ void main() {
 		#endif
 
 		if (lightDot > 0.01) { // the 0.01 here helps prevent against flickering on the north face of blocks
-			color.rgb *= torch_color * lmcoord.x + (ambient * 20) * sky + (1.0 - shadow);
-			color.rgb *= clamp(torch_color * lmcoord.x + (lightDot * sky * sky_light_color) + sky * 0.5+ ambient, 0. ,1.);
+			color.rgb *= (ambient * 20) * sky + (1.0 - shadow);
+			color.rgb *= clamp(torch_color * lmcoord.x + (lightDot * sky * sky_light_color) + sky * 0.5, 0. ,1.);
 		}
 
 		if (shadowPos == vec4(0.)) {
